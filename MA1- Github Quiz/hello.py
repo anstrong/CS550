317a7989b5f82a441b2371851efa4d70a11e94ad
Annabelle Strong <anstrong@users.noreply.github.com>
2017-12-04 08:14:58 -0500

    Add files via upload

---
+++ MA1- Github Quiz/hello.py
@@ -0,0 +1,15 @@
+#import sys 
+
+#print("Hello, world!")
+
+#print("Hello, ")
+#sys.stdout.write(sys.argv[1])
+#print("! Welcome.")
+
+
+import sys
+import stdio
+
+stdio.write('Hi, ')
+stdio.write(sys.argv[1])
+stdio.writeln('. How are you?')
